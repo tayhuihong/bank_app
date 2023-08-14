fe5b4188e96d652f7c19b0f8f5035fb484c655d3
tayhuihong <huihong1903@gmail.com>
2023-08-14 09:45:32 +0800

    update main,py
    added the logic for main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import bank_module
-import bank_model
+from bank_model import Account
+from account_model import Bank
 import datetime
 import random
+
+## Initiate 10 Account classes
+accounts = [
+    {'Amy': Account([], 500)},
+    {'Ben': Account([], 500)},
+    {'Charlie': Account([], 500)},
+    {'Dan': Account([], 500)},
+    {'Emily': Account([], 500)},
+    {'Frans': Account([], 500)},
+    {'Gufran': Account([], 500)},
+    {'Hendy': Account([], 500)},
+    {'Zoe': Account([], 500)},
+    {'Jill': Account([], 500)},
+]
+
+## Initiate Bank class with accounts
+bank = Bank(accounts)
+
 
